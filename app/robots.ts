3f2306fd0f65e9d60ac5e997da50ca4c08228329
“rcolabs-es” <contacto@rcolabs.es>
2025-07-10 11:52:46 +0200

    fixed robots

--- app/robots.ts
+++ app/robots.ts
@@ -2,10 +2,13 @@ import type { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/private/', '/admin/'],
+      },
+    ],
     sitemap: 'https://tcsystems.es/sitemap.xml',
     host: 'https://tcsystems.es',
   }
