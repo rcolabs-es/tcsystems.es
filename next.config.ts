693887ded342e1bad189db5bcf1991e91ca6f68c
adrianRacovitaJSX <rcolabsagency@gmail.com>
2025-11-07 17:09:26 +0100

    eslint disabled

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // Desactiva ESLint durante el build en producción
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // También ignora errores de TypeScript durante el build si es necesario
+    ignoreBuildErrors: false,
+  },
   images: {
     remotePatterns: [
       {
